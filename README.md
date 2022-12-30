d3fa069bc10adb536884e6be9cdbf0dee872722f
ctw_youyu <1508594093@qq.com>
2022-12-30 14:38:58 +0800

    Code updated 22-12-30 14:38:58

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ZGX_Public
 
-Season_Attack：
+Season_Attack文件夹：
+
 工会人员竞技场的进攻次数
+
 文件名为对应赛季
 
 
--- Update_GitHub.py
+++ Update_GitHub.py
@@ -7,19 +7,19 @@ def GetTime():
 
 commit_msg = f" \"Code updated {GetTime()}\" "
 
-cmd_1 = "git status"
+# cmd_1 = "git status"
 cmd_2 = "git add ."
 cmd_3 = "git commit -m" + commit_msg
 cmd_4 = "git push"
 # cmd_1 = ""
 
-print("start")
-os.system("cd..")
-print("[CMD]",cmd_1)
-os.system(cmd_1)
-print("[CMD]",cmd_2)
+# print("start")
+# os.system("cd..")
+# print("[CMD]",cmd_1)
+# os.system(cmd_1)
+# print("[CMD]",cmd_2)
 os.system(cmd_2)
-print("[CMD]",cmd_3)
+# print("[CMD]",cmd_3)
 os.system(cmd_3)
 
 result = 128
@@ -29,4 +29,4 @@ while (result != 0):
     print(result)
 
 print("END")
-time.sleep(100000)
+# time.sleep(100000)
