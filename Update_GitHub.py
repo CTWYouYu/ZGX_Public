0b79841b0f44ff3f38e3f693fbe77b7b94e61aba
ctw_youyu <1508594093@qq.com>
2022-12-30 15:19:08 +0800

    Code updated 22-12-30 15:19:07

--- Update_GitHub.py
+++ Update_GitHub.py
@@ -1,25 +1,19 @@
 import os,time
 
 
-
 def GetTime():
     return time.strftime("%y-%m-%d %H:%M:%S", time.localtime())
 
-commit_msg = f" \"Code updated {GetTime()}\" "
 
+if __name__ == '__main__':
+    commit_msg = f" \"Code updated {GetTime()}\" "
+    result = 128
+    cmd_2 = "git add ."
+    cmd_3 = "git commit -m" + commit_msg
+    cmd_4 = "git push"
+    os.system(cmd_2)
+    os.system(cmd_3)
 
-cmd_2 = "git add ."
-cmd_3 = "git commit -m" + commit_msg
-cmd_4 = "git push"
+    while (result != 0):
+        result = os.system(cmd_4)
 
-os.system(cmd_2)
-
-os.system(cmd_3)
-
-result = 128
-while (result != 0):
-    print("[CMD]", cmd_4)
-    result = os.system(cmd_4)
-    print(result)
-
-print("END")
